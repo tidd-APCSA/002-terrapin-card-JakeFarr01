128baff6b662f3ac1972ec14de4fa19b3968b4fc
Jake Farr <653645@aacps.org>
2020-10-29 03:28:05 +0000

    copied from other repl

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,31 @@
 public class TerrapinCard {
 
+  private double balance;
+
+  public TerrapinCard(double balance) {
+    this.balance = balance;
+  }
+
+  public void loadMoney(int money) {
+    if(money > 0) {
+      balance += money;
+    }
+  }
+
+  public void payEconomical() {
+    if(balance >= 2.5) {
+      balance -= 2.5;
+    }
+  }
+
+  public void payGourmet() {
+    if(balance >= 4) {
+      balance -= 4;
+    }
+  }
+
+  public String toString() {
+    return "The card has " + balance + " dollars";
+  }
 
 }
